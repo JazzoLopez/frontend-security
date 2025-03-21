c5bdaf98b849b4bb697a090c6f022074a5ca9706
JazzoLopez <jazzielrodriguezlopez@gmail.com>
2025-03-20 23:22:31 -0600

    Corregir la importación del componente User en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ import Login from './components/auth/login/Login'
 import Home from './components/dashboard/home/Home'
 import NotFound from './components/notFound/NotFound'
 import ProtectedRoutes from './routes/ProtectedRoutes'
-import User from '@components/dashboard/user/user'
+import User from '@components/dashboard/user/User'
+  
 import Loan from '@components/dashboard/loan/Loan'
 import Material from '@components/dashboard/material/Material'
+ 
 
 function App() {
   return (
